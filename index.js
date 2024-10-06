7809d1995eb6687189153fdcfc042212e86d0daf
Klaus Zerbe <klaus@zerbe.cloud>
2024-10-06 15:50:57 +0200

    update list structure

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const csvToJson = require('convert-csv-to-json')
 
 const app = express();
 
-let json = csvToJson.fieldDelimiter(',').parseSubArray('*',',')
-  .getJsonFromCsv(path.join(__dirname, './data/bp3.csv'));
+let json = csvToJson.fieldDelimiter(';').parseSubArray('*',',')
+  .getJsonFromCsv(path.join(__dirname, './data/bloodPressure.csv'));
 
 let timestamps = new Set()
 let values = [];
+
 
 const firstDate = new Date(json[0].Date).toLocaleDateString();
 const lastDate = new Date(json[json.length-1].Date).toLocaleDateString();
@@ -24,16 +25,20 @@ for (let idx=0; idx < json.length; idx++) {
     values.push({
       Time: dt, 
       Sys: Number(json[idx].SYS), 
-      SysColor: sysColor(json[idx].SYS),
+      SysFont: sysFontWeight(json[idx].SYS),
       Dia: Number(json[idx].DIA), 
-      DiaColor: diaColor(json[idx].DIA),
-      Puls: Number(json[idx].BPM)})
+      DiaFont: diaFontWeight(json[idx].DIA),
+      Puls: Number(json[idx].BPM),
+      Remark: json[idx].REM
+
+    })
 }
 
 let sumSys = 0, sumDia = 0, sumPuls = 0
 let minSys = 1000, minDia = 1000, minPuls = 1000
 let maxSys = 0, maxDia = 0, maxPuls = 0
 let avgSys = 0, avgdia = 0, avgPuls = 0
+
 
 for (let idx=0; idx<values.length; idx++) {
   sumSys += values[idx].Sys
@@ -66,24 +71,24 @@ app.get('/', (req, res) => res.render('table', {
     minPuls: minPuls, maxPuls: maxPuls, avgPuls: avgPuls
 }));
 
-app.listen(8000, () => console.log('Test running'))
+app.listen(8000, () => console.log('browse http://localhost:8000 for result'))
 
-function sysColor(num) {
+function sysFontWeight(num) {
   if (num < 130) {
-    return 'green'
+    return 200
   } else if (num < 140) {
-    return 'orange'
+    return 400
   } else {
-    return 'red'
+    return 600
   }
 }
 
-function diaColor(num) {
+function diaFontWeight(num) {
   if (num < 85) {
-    return 'green'
+    return 200
   } else if (num < 90) {
-    return 'orange'
+    return 400
   } else {
-    return 'red'
+    return 600
   }
 }
